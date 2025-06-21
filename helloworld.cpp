48c3446fd50f6fd48f85047188cffee2deb8c5ff
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-06-21 14:10:01 +0530

    Hello World in cpp

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // std = standard:: 
+    // cout = charOutput 
+    // << = output operator
+    // endl = end line (basically \n)
+    std::cout << "Hello World" << std::endl;
+
+    // wihtout endl = without \n)
+    std::cout << "Hello World";
+
+    return 0;
+}
