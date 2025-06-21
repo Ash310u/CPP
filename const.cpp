e01eb62d771fa8ea32b53919dbff209ddded515e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-06-21 15:52:37 +0530

    const EG

--- const.cpp
+++ const.cpp
@@ -4,7 +4,13 @@ int main() {
     // const is a keyword that is used to declare a constant variable
     // tells the compiler to prevent anything form modifying it. (read-only)
     const double pi = 3.14;
-    std::cout << "Number: " << pi << std::endl;
+    double radius = 5.0;
+    
+    double circumference = 2 * pi * radius;
+    double area = pi * radius * radius;
+    
+    std::cout << "Circumference: " << circumference << std::endl;
+    std::cout << "Area: " << area << std::endl;
     // pi = 100; // this will cause an error because pi is a constant
     // std::cout << "Number: " << pi << std::endl;
     return 0;
