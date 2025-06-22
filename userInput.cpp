cba7f581ada3c0d1d88742d700d9acde8ea0856f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-06-22 21:48:06 +0530

    update User Input in CPP

--- userInput.cpp
+++ userInput.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 
 int main() {
+    // Declaring variables to store user input
     std::string name;
     int age;
  
-    std::cout << "Enter your name: ";
-    std::cin >> name;
+    // Prompting user to enter their name
+    std::cout << "Enter your name: ";    
+    // Reading the full name including spaces using getline
+    // std::getline(std::cin, name);
     
+    // If we put a std::cin above getline will take '\n' as input and not the full name 
+    // and to prevent that we need to use std::ws to ignore any newline character or whitespace.
+    std::getline(std::cin >> std::ws, name);
+    
+    // Prompting user to enter their age
     std::cout << "Enter your age: ";
+    // Reading age as an integer
     std::cin >> age;
     
+    // Displaying personalized greeting with user's name and age
     std::cout << "Hello, " << name << "! You are " << age << " years old." << std::endl;
-    
+
     return 0;
 }
