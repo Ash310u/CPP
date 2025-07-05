ca5360dca76de4e712e574275caa1fc49042721e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-06 05:27:19 +0530

    Using Do While loop in CPP

---
+++ doWhileLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+    do {
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    } while (name.empty());
+    std::cout << "Hello, " << name << "!" << std::endl;
+    return 0;
+}
