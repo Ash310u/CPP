3a888bc708ab6bef9268593342c5159d05338648
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-06 05:48:23 +0530

    Implement nested for loops

--- forLoop.cpp
+++ forLoop.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        std::cout << i << std::endl;
+    // for loop
+    // for (int i = 1; i <= 10; i++) {
+    //     std::cout << i << std::endl;
+    // }
+
+    // Nested for loop
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
+            std::cout << i << " * " << j << " = " << i * j << std::endl;
+        }
     }
+
+
     return 0;
 }
