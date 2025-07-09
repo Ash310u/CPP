7c8b151394709a93f6502236eb59b3c753102093
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-09 06:09:26 +0530

    Implement FILL function from Algorithm library of CPP

--- fillFunc.cpp
+++ fillFunc.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <algorithm> // for std::fill
 
 int main() {
     int numbers[10];
     
-    // Fill the array with the value 5
+    // Fill the array with the value 5 int.
     std::fill(numbers, numbers + 10, 5);
     std::cout << "Array after fill: ";
     for (int i = 0; i < 10; ++i) {
@@ -14,6 +13,7 @@ int main() {
     
     std::string yoos[10];
     
+    // Fill the array with the value "Yoo..." string.
     std::fill(yoos, yoos + 10, "Yoo...");
     std::cout << "Array after fill: ";
     for (int i = 0; i < 10; ++i) {
