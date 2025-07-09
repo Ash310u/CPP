abcccc26e0e74c467a79eb679ae7b828eef570b3
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-09 05:41:26 +0530

    Implement Linear Search for Strings Using CPP

--- arraySearchFunc.cpp
+++ arraySearchFunc.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 
-int linearSearch(int arr[], int size, int target) {
+int linearSearch(std::string arr[], int size, std::string target) {
     for (int i = 0; i < size; i++) {
         if (arr[i] == target) {
             return i;
@@ -10,11 +10,11 @@ int linearSearch(int arr[], int size, int target) {
     return -1;
 }
 int main() {
-    int numbers[] = {1, 2, 3, 4, 5};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
+    std::string names[] = {"John", "Jane", "Jim", "Jill", "Jack"};
+    int size = sizeof(names) / sizeof(names[0]);
 
-    int target = 3;
-    int index = linearSearch(numbers, size, target);
+    std::string target = "Jim";
+    int index = linearSearch(names, size, target);
     if (index != -1) {
         std::cout << "Target found at index: " << index << std::endl;
     } else {
