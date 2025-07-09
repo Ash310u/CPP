8156497836606cb64a3ec611fcdd4a5185af36ee
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-09 06:16:22 +0530

    Created a array using FILL function with containing two diff strings

--- fillFunc.cpp
+++ fillFunc.cpp
@@ -5,18 +5,25 @@ int main() {
     
     // Fill the array with the value 5 int.
     std::fill(numbers, numbers + 10, 5);
+    
     std::cout << "Array after fill: ";
     for (int i = 0; i < 10; ++i) {
         std::cout << numbers[i] << " ";
     }
     std::cout << std::endl;
     
-    std::string yoos[10];
     
-    // Fill the array with the value "Yoo..." string.
-    std::fill(yoos, yoos + 10, "Yoo...");
+    const int size = 100;
+    std::string yoos[size];
+    
+    // Fill the array with the value "Yoo..." & "Loo..." string.
+    std::fill(yoos, yoos + (size/2), "Yoo...");
+    
+    // first arg is the start of the array, second arg is the end of the array.
+    std::fill(yoos + (size/2), yoos + size, "Loo...");
+    
     std::cout << "Array after fill: ";
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < size; ++i) {
         std::cout << yoos[i] << " ";
     }
     std::cout << std::endl;
