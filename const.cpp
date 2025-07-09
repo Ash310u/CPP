8593c64ebe42d9924f799611472c02d39491a811
Ash310u <107404699+Ash310u@users.noreply.github.com>
2025-07-09 17:41:59 +0530

    Used const inside function in CPP

--- const.cpp
+++ const.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 
+void printCircumferenceAndArea(const double &PI, const double radius);
+
 int main() {
     // const is a keyword that is used to declare a constant variable
     // tells the compiler to prevent anything form modifying it. (read-only)
     const double PI = 3.14;
     double radius = 5.0;
     
+    // pi = 100; // this will cause an error because pi is a constant
+    
+    printCircumferenceAndArea(PI, radius);
+    return 0;
+}
+
+void printCircumferenceAndArea(const double &PI, const double radius) {
     double circumference = 2 * PI * radius;
     double area = PI * radius * radius;
     
-    std::cout << "Circumference: " << circumference << std::endl;
     std::cout << "Area: " << area << std::endl;
-    // pi = 100; // this will cause an error because pi is a constant
-    // std::cout << "Number: " << pi << std::endl;
-    return 0;
+    std::cout << "Circumference: " << circumference << std::endl;
 }
